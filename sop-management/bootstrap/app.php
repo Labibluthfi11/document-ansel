30b4a43072880559bbc7f1bf3dee530e2728262f
Labibluthfi11 <muhammadlabibluthfitrpl@gmail.com>
2025-06-15 20:35:16 +0700

    yes

--- sop-management/bootstrap/app.php
+++ sop-management/bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Tambahkan TrustProxies di sini
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
